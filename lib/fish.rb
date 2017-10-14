e97669006902b0108a7db16c4a5a4b9f1ca4c8b6
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-14 19:54:45 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,14 +1,11 @@
 class Fish
   # code goes here
   attr_accessor :mood
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @mood = "nervous"
-  end
-
-  def name
-    @name.dub.freeze
   end
 
 end
