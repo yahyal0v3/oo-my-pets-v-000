5b59412f134a2f58d6dc0a52e471cf1c56239859
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-14 20:53:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,7 +58,7 @@ class Owner
   end
 
   def sell_pets
-    self.pets.each do |species, pets| 
+    self.pets.each do |species, pets|
       pets.each do |instance|
         instance.mood = "nervous"
       end
