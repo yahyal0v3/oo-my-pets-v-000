3a0420c3ca6d1936855618a9cb4d60c48be79acf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-14 20:29:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,17 +33,17 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    self.pets
+    self.pets[:fishes] = fish
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
-    self.pets
+    self.pets[:cats] = cat
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    self.pets
+    self.pets[:dogs] = dog
   end
 
 end
